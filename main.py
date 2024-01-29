15aa37c932926f9366c6f48a0ceedc8ac0a0fe3a
justi <jluo2@caltech.edu>
2024-01-28 17:33:11 -0800

    Add arguments

--- main.py
+++ main.py
@@ -3,22 +3,37 @@ from data_module import Caltech101DataModule
 from pytorch_lightning.loggers import WandbLogger
 from model import NeuralNetwork
 import wandb
+import argparse
 
-# Neural Network Parameters
-hidden_layers = [256]
-learning_rate = 0.001
-run_name = "hl-" + str(hidden_layers) + "-lr-" + str(learning_rate)
 
-pl.seed_everything(100)
-datamodule = Caltech101DataModule(test_split=0.1, valid_split=0.1)
+if __name__ == "__main__":
+    # Initialize parser
+    parser = argparse.ArgumentParser(description="Creates a NN model and trains it on the Caltech101 dataset")
+    parser.add_argument("-hl", "--HiddenLayers", help="List hidden layers, space separated (e.g. \"256 128\")")
+    parser.add_argument("-lr", "--LearningRate", help="Set Learning Rate (Default 0.001)")
+    args = parser.parse_args()
 
-model = NeuralNetwork(layers=hidden_layers, learning_rate=0.001)
+    # Neural Network Parameters
+    hidden_layers = [256]
+    if args.HiddenLayers:
+        hidden_layers = [int(l) for l in args.HiddenLayers.split()]
 
-wandb.login()
-wandb_logger = WandbLogger(project="mltask", name=run_name, log_model="all")
+    learning_rate = 0.001
+    if args.LearningRate:
+        learning_rate = float(args.LearningRate)
+    run_name = "hl-" + "-".join(hidden_layers) + "-lr-" + str(learning_rate)
 
-trainer = pl.Trainer(max_epochs=10, logger=wandb_logger)
-# Train the model
-trainer.fit(model, datamodule)
-trainer.test(model, datamodule)
-wandb.finish()
+    pl.seed_everything(100)
+
+    datamodule = Caltech101DataModule(test_split=0.1, valid_split=0.1)
+
+    model = NeuralNetwork(layers=hidden_layers, learning_rate=0.001)
+
+    wandb.login()
+    wandb_logger = WandbLogger(project="mltask", name=run_name, log_model="all")
+
+    trainer = pl.Trainer(max_epochs=10, logger=wandb_logger)
+    # Train the model
+    trainer.fit(model, datamodule)
+    trainer.test(model, datamodule)
+    wandb.finish()
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # CLIP Classifier for Caltech101
 
 This project uses the CLIP pretrained transform on top of a lightweight neural network to classify the [Caltech101](https://pytorch.org/vision/main/generated/torchvision.datasets.Caltech101.html) dataset.
+
+[Run in Colab]()
