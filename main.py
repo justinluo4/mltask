51dbcd50a7b9e63a518bbce814737e881b4dc934
justi <jluo2@caltech.edu>
2024-01-28 17:41:01 -0800

    Add arguments

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Creates a NN model and trains it on the Caltech101 dataset")
     parser.add_argument("-hl", "--HiddenLayers", help="List hidden layers, space separated (e.g. \"256 128\")")
     parser.add_argument("-lr", "--LearningRate", help="Set Learning Rate (Default 0.001)")
+    parser.add_argument("-e", "--Epochs", help="Set Max Epochs (default 10)")
     args = parser.parse_args()
 
     # Neural Network Parameters
@@ -23,6 +24,10 @@ if __name__ == "__main__":
         learning_rate = float(args.LearningRate)
     run_name = "hl-" + "-".join(hidden_layers) + "-lr-" + str(learning_rate)
 
+    max_epochs = 10
+    if args.Epochs:
+        max_epochs = int(args.Epochs)
+
     pl.seed_everything(100)
 
     datamodule = Caltech101DataModule(test_split=0.1, valid_split=0.1)
@@ -32,7 +37,7 @@ if __name__ == "__main__":
     wandb.login()
     wandb_logger = WandbLogger(project="mltask", name=run_name, log_model="all")
 
-    trainer = pl.Trainer(max_epochs=10, logger=wandb_logger)
+    trainer = pl.Trainer(max_epochs=max_epochs, logger=wandb_logger)
     # Train the model
     trainer.fit(model, datamodule)
     trainer.test(model, datamodule)
--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 
 This project uses the CLIP pretrained transform on top of a lightweight neural network to classify the [Caltech101](https://pytorch.org/vision/main/generated/torchvision.datasets.Caltech101.html) dataset.
 
-[Run in Colab]()
+[Run in Colab](https://githubtocolab.com/justinluo4/mltask/blob/master/notebooks/run.ipynb)
